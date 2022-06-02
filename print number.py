4a5b3ae14a6c257f78c62797b20cff273ee1eb94
Your Name <you@example.com>
2022-06-02 23:29:22 +0530

    alarm tik tok

---
+++ print number.py
@@ -0,0 +1,8 @@
+# Python program to print numbers from 1 to n
+
+n = int(input("Please Enter any Number: "))
+
+print("The List of Natural Numbers from 1", "to", n)
+
+for i in range(1, n + 1):
+    print(i, end = '  ')
